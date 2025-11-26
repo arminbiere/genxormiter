b9ae3e938878004c4e4c78d4f113ff06886f43a2
Armin Biere <armin.biere@gmail.com>
2025-11-26 13:58:58 +0100

    got almost it

--- genxormiter.c
+++ genxormiter.c
@@ -19,11 +19,50 @@ static char  * usage =
 #include <time.h>
 #include <unistd.h>
 
-#define INPUTS_MAX (INT_MAX / 3)
+// We need
+//
+//   variables <= INT_MAX
+//
+// and for 'inputs > 1' we have
+//
+//    temporaries = 2*(inputs - 1)
+//      variables =  inputs + temporaries
+//
+// thus
+//
+//       INT_MAX >= variables
+//                = inputs + 2*(inputs-1)
+//                = 3*inputs - 2
+//
+// which yields
+//
+//   INT_MAX + 2 >= 3*inputs
+//
+// and finally
+//
+//        inputs <= (INT_MAX+2)/3
+
+#define INPUTS_MAX ((int)(((INT_MAX + 2u) / 3)))
+
+// This gives with 32-bit two-complement
+//
+//       INT_MAX = 2^31 - 1 = 2147483647
+//    INPUTS_MAX = (2147483647 + 2)/3 = 2147483649/3 = 715827883
+//
+// and note that
+//
+//     INT_MAX/3 = 2147483647/3 = 715827882
+//
+// as INT_MAX is divisible by 3.  That would be off by one.
 
 static int inputs;
+static int temporaries;
+static int variables;
+static size_t clauses;
+static size_t xors;
+
 static uint64_t seed;
-static bool verbose;
+static int verbose;
 
 static uint64_t next(void) {
   return seed = seed * 6364136223846793005ul + 1442695040888963407ul;
@@ -34,24 +73,9 @@ static int pick(unsigned mod) { return (mod / 4294967296.0) * (next() >> 32); }
 static bool flip(void) { return pick(2); }
 
 struct clause {
-  int size;
-  int lits[4];
+  int lits[3];
 };
 
-#if 0
-static void swap(int *begin, int i, int n) {
-  assert(0 <= i), assert(i < n);
-  if (!i)
-    return;
-  int j = pick(i + 1u);
-  if (i == j)
-    return;
-  assert(0 <= j), assert(j < n);
-  int tmp = begin[i];
-  begin[i] = begin[j];
-  begin[j] = tmp;
-}
-#else
 #define SWAP(BEGIN, I, N)                                                      \
   do {                                                                         \
     assert(0 <= I), assert(I < N);                                             \
@@ -67,7 +91,6 @@ static void swap(int *begin, int i, int n) {
     memcpy(BEGIN + I, BEGIN + J, BYTES);                                       \
     memcpy(BEGIN + J, TMP, BYTES);                                             \
   } while (0)
-#endif
 
 static void die(const char *fmt, ...) {
   va_list ap;
@@ -91,7 +114,7 @@ static void parse_inputs(const char *arg) {
   while ((ch = *p++)) {
     if (INPUTS_MAX / 10 < inputs)
     NUMBER_TOO_LARGE:
-      die("number of inputs '%s' too large", arg);
+      die("number of inputs '%s' too large (maximum '%d')", arg);
     inputs *= 10;
     int digit = ch - '0';
     if (INPUTS_MAX - digit < inputs)
@@ -121,6 +144,39 @@ static void parse_seed(const char *arg) {
   }
 }
 
+static void valid(int lit) {
+  assert(lit);
+  assert(lit != INT_MIN);
+  assert(abs(lit) <= variables);
+}
+
+static void ternary(struct clause *c, int lit0, int lit1, int lit2) {
+  valid(lit0), valid(lit1), valid(lit2);
+  int lits[3] = {lit0, lit1, lit2};
+  for (int k = 0; k != 3; k++)
+    SWAP(lits, k, 3);
+  if (verbose > 1)
+    printf("c c[%zu] %d %d %d\n", clauses, lits[0], lits[1], lits[2]);
+  memcpy(c[clauses].lits, lits, sizeof lits);
+  clauses++;
+}
+
+static void XOR(struct clause *c, int lhs, int rhs0, int rhs1) {
+  if (flip())
+    lhs = -lhs, rhs0 = -rhs0;
+  if (flip())
+    lhs = -lhs, rhs1 = -rhs1;
+  if (flip())
+    rhs0 = -rhs0, rhs1 = -rhs1;
+  if (verbose)
+    printf("c x[%zu] %d = %d ^ %d\n", xors, lhs, rhs0, rhs1);
+  ternary(c, lhs, rhs0, -rhs1);
+  ternary(c, lhs, -rhs0, rhs1);
+  ternary(c, -lhs, rhs0, rhs1);
+  ternary(c, -lhs, -rhs0, -rhs1);
+  xors++;
+}
+
 int main(int argc, char **argv) {
 
   const char *seed_option = 0;
@@ -132,7 +188,7 @@ int main(int argc, char **argv) {
       fputs(usage, stdout);
       exit(0);
     } else if (!strcmp(arg, "-v") || !strcmp(arg, "--verbose")) {
-      verbose = true;
+      verbose += (verbose < 2);
     } else if (arg[0] == '-')
       die("invalid option '%s' (try '-h'", arg);
     else if (seed_option)
@@ -145,14 +201,6 @@ int main(int argc, char **argv) {
       parse_seed(arg);
       seed_option = arg;
     }
-  }
-
-  int *s[2];
-
-  for (int i = 0; i != 2; i++) {
-    s[i] = malloc(inputs * sizeof *s[i]);
-    if (inputs && !s[i])
-      die("out-of-memory allocating stack of variables");
   }
 
   if (!seed_option) {
@@ -169,83 +217,89 @@ int main(int argc, char **argv) {
     return 0;
   }
 
-  for (int i = 0; i != 2; i++)
-    for (int j = 0; j != inputs; j++) {
-      int lit = j + 1;
-      if (flip())
-        lit = -lit;
-      s[i][j] = lit;
-    }
+  if (inputs == 1) {
+    int lit = flip() ? -1 : 1;
+    printf("p cnf 1 2\n");
+    printf("%d 0\n", lit);
+    printf("%d 0\n", -lit);
+    return 0;
+  }
 
-  for (int i = 0; i != 2; i++)
-    for (int j = 0; j != inputs; j++)
-      SWAP(s[i], j, inputs);
+  temporaries = 2 * (inputs - 1);
+  variables = inputs + temporaries;
 
-  if (verbose)
-    for (int i = 0; i != 2; i++) {
-      printf("c s[%d] inputs\n", i);
-      for (int j = 0; j != inputs; j++)
-        printf("c s[%d][%d] = %d\n", i, j, s[i][j]);
-    }
+  int *m = malloc(variables * sizeof *m);
+  if (inputs && !m)
+    die("out-of-memory allocating variables");
 
-  const int temporaries = inputs - 1;
-  int *t[2] = {0, 0};
-
-  for (int i = 0; i != 2; i++)
-    if (inputs > 1 && !(t[i] = malloc((temporaries) * sizeof *t[i])))
-      die("out-of-memory allocating temporary stack of variables");
-
-  int n[2] = {0, 0};
-
-  int variables = inputs;
-
-  while (n[0] != temporaries || n[1] != temporaries) {
-    int i;
-    if (n[0] == temporaries)
-      i = 1;
-    else if (n[1] == temporaries)
-      i = 0;
-    else
-      i = flip();
-    assert(n[i] < temporaries);
-    int lit = ++variables;
+  for (int j = 0; j != variables; j++) {
+    int lit = j + 1;
     if (flip())
       lit = -lit;
-    t[i][n[i]++] = lit;
+    m[j] = lit;
+    SWAP(m, j, variables);
   }
-  for (int i = 0; i != 2; i++)
-    for (int j = 0; j != temporaries; j++)
-      SWAP(s[i], j, temporaries);
 
-  if (verbose)
-    for (int i = 0; i != 2; i++) {
-      printf("c t[%d] temporaries\n", i);
-      for (int j = 0; j != n[i]; j++)
-        printf("c t[%d][%d] = %d\n", i, j, t[i][j]);
-    }
+  if (verbose) {
+    for (int j = 0; j != inputs; j++)
+      printf("c m[%d] = input[%d] = %d\n", j, j, m[j]);
+    for (int j = inputs; j != variables; j++)
+      printf("c m[%d] = temporary[%d] = %d\n", j, j - inputs, m[j]);
+  }
 
-  size_t clauses = 8 * (temporaries) + 2;
-  struct clause *c = malloc(clauses * sizeof *c);
+  struct clause *c = malloc(4 * temporaries * sizeof *c);
   if (!c)
     die("out-of-memory allocating clauses");
 
-  int m[2] = {inputs, inputs};
-  while (m[0] > 1 || m[1] > 1) {
+  int *s[2];
+  for (int i = 0; i != 2; i++) {
+    if (!(s[i] = malloc(inputs * sizeof *s[i])))
+      die("out-of-memory allocating %s stack", i ? "first" : "second");
+    for (int j = 0; j != inputs; j++) {
+      s[i][j] = m[j];
+      SWAP(s[i], j, inputs);
+    }
+  }
+
+  if (verbose)
+    for (int i = 0; i != 2; i++)
+      for (int j = 0; j != inputs; j++)
+        printf("c s[%d][%d] = input[%d] = %d\n", i, j, i, s[i][j]);
+
+  int n[2] = {inputs, inputs};
+  assert(n[0]), assert(n[1]);
+  int temporary = inputs;
+  while (n[0] + n[1] > 3) {
     int i;
-    if (m[0] == 1)
+    if (n[0] == 1)
       i = 1;
-    else if (m[1] == 1)
+    else if (n[1] == 1)
       i = 0;
     else
       i = flip();
-    assert(m[i] > 1);
-    break;
+    assert(n[i] >= 2);
+    int lhs = m[temporary++], rhs[2];
+    for (int k = 0; k != 2; k++) {
+      SWAP(s[i], n[i] - 1, n[i]);
+      rhs[k] = s[i][--n[i]];
+    }
+    XOR(c, lhs, rhs[0], rhs[1]);
+    assert(n[i] < inputs);
+    s[i][n[i]++] = lhs;
   }
+  assert(n[0] + n[1] == 3);
+  assert(temporary + 1 == variables);
 
   printf("p cnf %d %zu\n", variables, clauses);
 
+  int i = (n[0] == 1);
+  assert(n[i] == 2);
+  assert(n[!i] == 1);
+
   for (int i = 0; i != 2; i++)
-    free(s[i]), free(t[i]);
+    free(s[i]);
+
+  free(m);
   free(c);
 
   return 0;
